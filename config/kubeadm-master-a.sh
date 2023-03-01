ebb6deedd52ecb07a7fa1f66e11c22227275b3fc
JosephYostos <86612715+JosephYostos@users.noreply.github.com>
2023-03-01 11:34:37 +0000

    Update kubeadm-master-a.sh

--- config/kubeadm-master-a.sh
+++ config/kubeadm-master-a.sh
@@ -60,3 +60,4 @@ mkdir -p $HOME/.kube
 sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
 sudo chown $(id -u):$(id -g) $HOME/.kube/config
 kubectl taint nodes --all node-role.kubernetes.io/master-
+kubectl taint nodes --all node-role.kubernetes.io/control-plane-
